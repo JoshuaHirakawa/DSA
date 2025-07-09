4bf1e988f82ba3654bf1b9543f57806e59770105
Joshua Hirakawa <joshuahirakawa@gmail.com>
2025-07-09 00:11:49 -0700

    move zeros

--- Leetcode/easy/moveZeros.js
+++ Leetcode/easy/moveZeros.js
@@ -20,5 +20,28 @@ Output: [0]
 
 // input: array of nums
 // output: same array with 0's at end
-// strat:
-var moveZeros = function (nums) {};
+// strategy: have two pointers right = 1 and left = 0, if num at left = 0 but right doesnt, swap. If both nums equal 0, increment right
+var moveZeros = function (nums) {
+  //define a left and right pointer
+  let left = 0,
+    right = 1;
+
+  // iterate as long as right is less than array length
+  while (right < nums.length) {
+    // swap if only left num is 0
+    if (nums[left] === 0 && nums[right] !== 0) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left++;
+    } // if both are 0, just increment right
+    else if (nums[left] === 0 && nums[right] === 0) right++;
+    // if both are non-zeros increment both
+    else {
+      left++;
+      right++;
+    }
+  }
+  return nums;
+};
+
+console.log(moveZeros([0, 1, 0, 3, 12, 0, 0, 2, 3, 4, 5, 0, 5]));
+console.log(moveZeros([1, 0, 1]));
